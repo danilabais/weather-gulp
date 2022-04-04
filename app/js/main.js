a9ab80e3a86520da73e09df0269a6c8fe62108bf
Danila <da_buddha@bk.ru>
2022-04-04 12:08:21 +0300

    Адаптив сделан

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,30 @@
 const swiper = new Swiper('.content', {
-    // Optional parameters
-    //loop: true,
-  
-    // If we need pagination
-   
-  
-    // Navigation arrows
-    
     mousewheel: {
       sensitivity:1,
     },
     speed:350,
-    slidesPerView:2.6,
+    
     watchOverflow:true,
     centeredSlides: true,
     grabCursor:true,
     slideToClickedSlide:true,
-    spaceBetween: 250,
-   
+    spaceBetween: 20,
+    
+    breakpoints: {
+     
+      0: {
+        slidesPerView: 1.2,
+        spaceBetween: 0,
+      },
+      800: {
+        slidesPerView: 2.4,
+        spaceBetween: 100,
+      },
+      1400: {
+        slidesPerView:2.6,
+        spaceBetween: 240,
+      },
+    }
    
    
   });
